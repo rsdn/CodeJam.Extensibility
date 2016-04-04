f0b157a880c7c967cc9efe00bc7af6716b3ba8ad
andrewvk <avk@rsdn.ru>
2016-04-04 16:03:20 +0300

    Push to official nuget feed

---
+++ src/Compile.cmd
@@ -0,0 +1,30 @@
+﻿"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.0\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.0\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.0\   /property:TargetFrameworkVersion=v4.0 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40" -p:FrameworkPathOverride="%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.0\ /property:TargetFrameworkVersion=v4.0 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"  -p:FrameworkPathOverride="%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.5\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5\   /property:TargetFrameworkVersion=v4.5 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45" 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.5\ /property:TargetFrameworkVersion=v4.5 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"  
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5.1\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.5.1\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5.1\   /property:TargetFrameworkVersion=v4.5.1 /p:DefineConstants="FW461;FW46;FW452;FW451" 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.5.1\ /property:TargetFrameworkVersion=v4.5.1 /p:DefineConstants="FW461;FW46;FW452;FW451"  
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5.2\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.5.2\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5.2\   /property:TargetFrameworkVersion=v4.5.2 /p:DefineConstants="FW461;FW46;FW452" 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.5.2\ /property:TargetFrameworkVersion=v4.5.2 /p:DefineConstants="FW461;FW46;FW452"  
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.6\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.6\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.6\   /property:TargetFrameworkVersion=v4.6 /p:DefineConstants="FW461;FW46" 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.6\ /property:TargetFrameworkVersion=v4.6 /p:DefineConstants="FW461;FW46"  
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.6.1\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.6.1\
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.6.1\   /property:TargetFrameworkVersion=v4.6.1 /p:DefineConstants="FW461" 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.6.1\ /property:TargetFrameworkVersion=v4.6.1 /p:DefineConstants="FW461"  
+

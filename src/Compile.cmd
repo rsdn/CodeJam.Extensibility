df5a94dfdd3cef64c4a98e38176fe4da477a30ed
andrewvk <avk@rsdn.ru>
2016-04-04 16:16:15 +0300

    Fix package build

--- src/Compile.cmd
+++ src/Compile.cmd
@@ -1,9 +1,4 @@
-﻿"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.0\
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.0\
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.0\   /property:TargetFrameworkVersion=v4.0 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40" -p:FrameworkPathOverride="%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.0\ /property:TargetFrameworkVersion=v4.0 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45;FW40"  -p:FrameworkPathOverride="%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
-
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5\
+﻿"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5\
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:Clean   /property:Configuration=Release /property:OutputPath=bin\Release.4.5\
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.5\   /property:TargetFrameworkVersion=v4.5 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45" 
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" CodeJam.Extensibility.csproj /target:ReBuild /property:Configuration=Release /property:OutputPath=bin\Release.4.5\ /property:TargetFrameworkVersion=v4.5 /p:DefineConstants="FW461;FW46;FW452;FW451;FW45"  
